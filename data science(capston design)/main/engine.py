8be6ecad61d6806e76687fbf7ba80c479aa6e84f
Bangjeho <fix9111@naver.com>
2019-11-19 00:39:52 +0900

    make UI and final update

---
+++ data science(capston design)/main/engine.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f3f0dd8f15bc1a865e76a1d4e690580d25644fb41faff5d1a608425a64e8d95c
+size 1507
---
+++ data science(capston design)/main/resultUI.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e6b5acbc07f6b19d58a77d84caeb4d166844a655427381f8b6b32c000e11f7d
+size 697
